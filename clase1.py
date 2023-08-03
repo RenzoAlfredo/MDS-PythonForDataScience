abdfc276d118aa3cc0969286cbe9f961b77ae40e
Renzo H <renzo.huamanyauri.h@uni.pe>
2023-08-02 21:25:13 -0500

    clase 1

--- clase1.py
+++ clase1.py
@@ -1,10 +1,53 @@
 
 #listas
 a = [1, 2, 3]
-
 print(a)
 
+a.append(4)
 
+b = ['a','b','c']
+print(b)
+
+#tuplas -
+
+#diccionarios
+
+#set
+
+
+#operadores de comparacion
+print(2 > 5)
+
+
+# if
+
+# for
+seq = [1, 2, 3, 4, 5]
+for item in seq:
+    print(item)
+
+# while
+i = 1
+while i < 5:
+    print(f'i es {i}')
+    #i = i + 1
+    i += 1
+
+# range
+r = range(5)
+print(r)
+for i in r:
+    print(i)
+
+# funcion
+def square(x):
+    return x**2
+
+print(square(5))
+
+
+# texto
+a = 'esta es la primera clase'
 
 
 
---
+++ clase1_1.py
@@ -0,0 +1,11 @@
+
+# texto
+a = 'esta es la primera clase'
+
+print(a.upper())
+
+b = a.split()
+
+print(b)
+print(b[3])
+
